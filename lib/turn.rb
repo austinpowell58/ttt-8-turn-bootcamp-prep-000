7dcce10ba77f884418acd30936a453c0238809ca
Austin Powell <austinpowell58@gmail.com>
2018-07-22 02:49:58 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if (board[index] == " " || board[index] == NIL || board[index] == "") && index.between?(0, 8)
+    return TRUE
+  else
+    return FALSE
+  end
+end
+
+def input_to_index(user_input = 0)
+  return user_input.to_i - 1
+end 
+
+def move(board, index, character = 'X')
+  return board[index] = character
+end
+  
+def turn(board)
+  puts "Please enter 1-9:"
+end 
